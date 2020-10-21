e1d2b63a15d8f092a314c2f409fe79cb6f1b473f
Suraj Ghodke <surajghodke0102@gmail.com>
2020-10-22 00:11:46 +0530

    Updates

---
+++ precache-manifest.7b1b209626c37d5a6160510ec172ad25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170900b7233df38628f71faa6b6fe5c3",
+    "url": "/keep_clone/index.html"
+  },
+  {
+    "revision": "430614050c4f269116bf",
+    "url": "/keep_clone/static/css/main.74f5e673.chunk.css"
+  },
+  {
+    "revision": "1eb17887b862b0577852",
+    "url": "/keep_clone/static/js/2.aa1484d0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keep_clone/static/js/2.aa1484d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430614050c4f269116bf",
+    "url": "/keep_clone/static/js/main.ca6cb119.chunk.js"
+  },
+  {
+    "revision": "b256393038bd01cdb50f",
+    "url": "/keep_clone/static/js/runtime-main.9e173218.js"
+  }
+]);
